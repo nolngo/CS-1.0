6e07b0b31365e88d842df55d11f44df1ccbb59b3
nolngo <nolanngo@protonmail.com>
2020-09-06 21:37:08 -0700

    added functions

---
+++ functions.py
@@ -0,0 +1,17 @@
+def area(width, height):
+    result = width * height
+    return result
+result = area(5, 6)
+print(result)
+result = area(2, 42)
+print(result)
+result = area(32, 6)
+print(result)
+
+def subtract(num1, num2):
+    result=num1-num2
+    return result
+result = subtract(3, 43)
+print(result)
+result = subtract(34, 17)
+print(result)
