ffe1029370ccb8d06a3a21c957f8f8291d289e1f
Nolan <60010605+nolngo@users.noreply.github.com>
2020-09-22 21:26:54 -0700

    2nd commit

--- chatbot.py
+++ chatbot.py
@@ -2,8 +2,8 @@ from random import choice
 
 def scary_movie_bot(user_response):
 
-  bot_response_supernatural = ["It's all fake!", "Watch a behind the scenes video!", "I love to see you happy!"]
-  bot_response_thriller = ["you are in a safe place!", "sending good vibes"]
+  bot_response_supernatural = ["It's all fake!", "Watch a behind the scenes video!", "Go watch some peppa pig!"]
+  bot_response_thriller = ["Stop watching thrillers! Pingu is a much more entertaining show", "The chances of that ever happenning to you is right around 0", "Go watch an episode of spongebob!"]
 
   if user_response == "supernatural":
     return choice(bot_response_supernatural)
@@ -25,5 +25,5 @@ while True:
     break
 
   
-  bot_response = get_mood_bot_response(user_response)
+  bot_response = def scary_movie_bot(user_response)
   print(bot_response)
